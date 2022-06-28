87c924c409596ad65ad48ee6d376812f6d105cd6
Edgar Alejandro Lopez <edalopez@sura.com.co>
2022-06-28 01:19:14 -0500

    Ajustes en el calculo del valor total de las materias del estudiante

--- algoritmo1.js
+++ algoritmo1.js
@@ -22,8 +22,9 @@ class Estudiante {
         this.nombreEstudiante = nombreEstudiante;
         this.cantidadMaterias = cantidadMaterias;
         this.materias = [];
-        const costoFijoPapeleria = 20000;
-        const costoFijoCarnet = 8000;
+        this.costoFijoPapeleria = 20000;
+        this.costoFijoCarnet = 8000;
+        this.porcentajeDescuento = 20;
     }
 
     nuevaMateria(materia, costo) {
@@ -40,14 +41,20 @@ class Estudiante {
         this.materias.forEach(materia => {
             valorTotal += materia.getCosto();
         });
-        return valorTotal - (costoFijoPapeleria + costoFijoCarnet);
+        return (valorTotal - (valorTotal/100)*this.porcentajeDescuento) + (this.costoFijoPapeleria + this.costoFijoCarnet);
     }
     
 }
 
 document.getElementById("botonValorTotal").addEventListener('click', function() {
 
+    let estudiante = new Estudiante("Alejo Lopez", 4);
+    estudiante.nuevaMateria("Ingles", 200000);
+    estudiante.nuevaMateria("Matemáicas", 200000);
+    estudiante.nuevaMateria("Español", 200000);
+    estudiante.nuevaMateria("Educación fisica", 200000);
 
+    document.getElementById("mostrarValorTotal").innerHTML = `El vaor total es ${estudiante.getValorTotalConDescuento()}`;
 
 });
 
